9a2bc0cd24c2c40b41524f7f70e8f9eb3b21abaa
Gaganjeet <gaganjeetwork99@gmail.com>
2024-12-01 03:11:43 +0530

    testing the app for inital phase

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,9 @@
 # quiz/urls.py
 from django.urls import path
+from . import views
 from .views import dashboard
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('quiz/', views.quiz, name='quiz'),
 ]
