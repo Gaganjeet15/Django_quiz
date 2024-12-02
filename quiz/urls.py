9bb1a0192f0f300478e292891b2382268a554f6c
Gaganjeet <gaganjeetwork99@gmail.com>
2024-12-02 22:40:13 +0530

    fixed login url

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,11 +1,9 @@
-# quiz/urls.py
 from django.urls import path
 from . import views
-from .views import dashboard
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # Default route to login
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('', views.login_view, name='login'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),  # Changed from dashboard to dashboard_view
     path('quiz/', views.quiz, name='quiz'),
-    path('reset-progress/', views.reset_progress, name='reset_progress'),
+    path('reset/', views.reset_progress, name='reset_progress'),
 ]
