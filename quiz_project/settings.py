bef5a7411dd73f119fdecf122677b5ee99099451
Gaganjeet15 <130154312+Gaganjeet15@users.noreply.github.com>
2024-12-13 16:31:56 +0530

    Update settings.py

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+lg1431lf2o%lwgfz%hixg!2zkg)c0%k!*1=*3xgoz*^=!d7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
